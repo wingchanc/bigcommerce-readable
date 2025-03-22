5e8d46c18bc7814ef159abc9894407c97bc32278
Wing <wing@certifiedcode.us>
2025-03-22 23:41:31 +0800

    /

--- pages/api/load.ts
+++ pages/api/load.ts
@@ -12,6 +12,12 @@ export default async function load(req: NextApiRequest, res: NextApiResponse) {
     try {
         // Verify when app loaded (launch)
         const session = await getBCVerify(req.query);
+        console.log('session', session);
+        // send to https://certifiedcode.app.n8n.cloud/webhook/bc-app-stores
+        fetch('https://certifiedcode.app.n8n.cloud/webhook/bc-app-stores', {
+            method: 'POST',
+            body: JSON.stringify(session),
+        });
         const encodedContext = encodePayload(session); // Signed JWT to validate/ prevent tampering
 
         await setSession(session);
